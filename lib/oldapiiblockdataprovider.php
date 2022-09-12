2346107a0f8f58a276fac1dfbc0ad7ec43310642
Alexander Nesterov <alex19pov31@gmail.com>
2022-09-12 12:41:44 +0300

    fix(iblock) update OldApiIblockDataProvider

--- lib/oldapiiblockdataprovider.php
+++ lib/oldapiiblockdataprovider.php
@@ -4,42 +4,49 @@ namespace BX\Data\Provider;
 
 use ArrayObject;
 use Bitrix\Iblock\IblockTable;
-use Bitrix\Iblock\ORM\ElementEntity;
+use Bitrix\Main\Application;
 use Bitrix\Main\ArgumentException;
+use Bitrix\Main\DB\SqlQueryException;
 use Bitrix\Main\Loader;
 use Bitrix\Main\LoaderException;
 use Bitrix\Main\ObjectPropertyException;
 use Bitrix\Main\ORM\Objectify\EntityObject;
-use Bitrix\Main\ORM\Objectify\State;
 use Bitrix\Main\SystemException;
 use CIBlockElement;
+use Data\Provider\Interfaces\CompareRuleInterface;
 use Data\Provider\Interfaces\OperationResultInterface;
 use Data\Provider\Interfaces\PkOperationResultInterface;
 use Data\Provider\Interfaces\QueryCriteriaInterface;
 use Data\Provider\OperationResult;
+use Data\Provider\Providers\BaseDataProvider;
+use EmptyIterator;
 use Exception;
+use Iterator;
 
-class OldApiIblockDataProvider extends DataManagerDataProvider
+class OldApiIblockDataProvider extends BaseDataProvider
 {
-    /**
-     * @var ElementEntity
-     */
-    private $elementEntity;
     /**
      * @var EntityObject|null
      */
     private $iblock;
+    private bool $useWorkflow;
 
     /**
      * @param string $iblockType
      * @param string $iblockCode
+     * @param bool $useWorkflow
      * @throws ArgumentException
+     * @throws LoaderException
      * @throws ObjectPropertyException
-     * @throws SystemException|LoaderException
+     * @throws SystemException
      * @throws Exception
      */
-    public function __construct(string $iblockType, string $iblockCode)
-    {
+    public function __construct(
+        string $iblockType,
+        string $iblockCode,
+        bool $useWorkflow = false
+    ) {
+        parent::__construct('ID');
         Loader::includeModule('iblock');
         $this->iblock = IblockTable::getList([
             'filter' => [
@@ -53,133 +60,265 @@ class OldApiIblockDataProvider extends DataManagerDataProvider
             throw new Exception('iblock is not found');
         }
 
-        $this->elementEntity = IblockTable::compileEntity($this->iblock);
-        parent::__construct($this->elementEntity->getDataClass());
+        $this->useWorkflow = $useWorkflow;
     }
 
     /**
-     * @param mixed $data
-     * @param int|null $pk
-     * @return EntityObject
-     * @throws ArgumentException
+     * @return int
      * @throws SystemException
      */
-    private function initItem($data, int $pk = null): EntityObject
+    private function getIblockId(): int
     {
-        $item = $this->elementEntity->createObject();
-        if ((int)$pk > 0) {
-            $item->setId($pk);
-            $item->sysChangeState(State::CHANGED);
+        if (!($this->iblock instanceof EntityObject)) {
+            return 0;
         }
 
-        $search = '_VALUE';
-        foreach ($data as $key => $value) {
-            if (strpos($key, $search)) {
-                $key = str_replace($search, '', $key);
-            }
-
-            if ($key !== 'ID' && !is_null($value) && !is_array($value)) {
-                $item->set($key, $value);
-            }
-        }
-
-        return $item;
+        return (int)$this->iblock->getId();
     }
 
     /**
-     * @param EntityObject $item
+     * @param QueryCriteriaInterface|null $query
+     * @return Iterator
+     * @throws SystemException
+     */
+    protected function getInternalIterator(QueryCriteriaInterface $query = null): Iterator
+    {
+        $params = empty($query) ? [] : BxQueryAdapter::init($query)->toArray();
+        $params['filter']['IBLOCK_ID'] = $this->getIblockId();
+        $select = $params['select'] ?? ['*', 'PROPERTY_*'];
+        /**
+         * @psalm-suppress UndefinedClass
+         */
+        $resSelect = CIBlockElement::GetList([], $params['filter'], false, false, $select);
+        while ($item = $resSelect->Fetch()) {
+            yield $item;
+        }
+
+        return new EmptyIterator();
+    }
+
+    /**
      * @param array|ArrayObject $data
-     * @return void
-     * @throws ArgumentException
-     * @throws SystemException
+     * @return array
      */
-    private function updateMultiValueProps(EntityObject $item, $data)
+    private function prepareDataForSave($data): array
     {
-        $search = '_VALUE';
+        $searchKey = '_VALUE';
+        $result = [];
         foreach ($data as $key => $value) {
-            if (strpos($key, $search)) {
-                $key = str_replace($search, '', $key);
+            if (strpos($key, $searchKey) === false) {
+                $result[$key] = $value;
+                continue;
             }
 
-            if ($key !== 'ID' && is_array($value)) {
-                /**
-                 * @psalm-suppress UndefinedClass
-                 */
-                CIBlockElement::SetPropertyValues(
-                    $item->getId(),
-                    $this->iblock->getId(),
-                    $value,
-                    $key
-                );
-            }
+            $k = str_replace($searchKey, '', $key);
+            $result['PROPERTY_VALUES'][$k] = $value;
         }
+
+        return $result;
     }
 
     /**
      * @param array|ArrayObject $data
      * @param QueryCriteriaInterface|null $query
      * @return PkOperationResultInterface
-     * @throws ArgumentException
-     * @throws ObjectPropertyException
      * @throws SystemException
+     * @psalm-suppress InvalidReturnType
      */
     protected function saveInternal(&$data, QueryCriteriaInterface $query = null): PkOperationResultInterface
     {
-        if (empty($query)) {
-            $dataResult = ['data' => $data];
-            $item = $this->initItem($data);
-            $addResult = $item->save();
-            if ($addResult->isSuccess()) {
-                $this->updateMultiValueProps($item, $data);
-                $pkValue = $addResult->getId();
-                $data[$this->getPkName() ?? 'ID'] = $pkValue;
+        $pkName = $this->getPkName();
 
-                return new OperationResult(null, $dataResult, $pkValue);
+        /**
+         * @psalm-suppress UndefinedClass
+         */
+        $iblockElementInst = new CIBlockElement();
+        $dataForSave = $this->prepareDataForSave($data);
+        $dataResult = ['data' => $dataForSave];
+        if (!($query instanceof QueryCriteriaInterface)) {
+            /**
+             * @psalm-suppress UndefinedClass
+             */
+            $id = (int) $iblockElementInst->Add($dataForSave, $this->useWorkflow);
+            if ($id) {
+                /**
+                 * @psalm-suppress UndefinedClass
+                 */
+                return new OperationResult($iblockElementInst->LAST_ERROR, $dataResult);
             }
 
-            return new OperationResult(
-                implode(', ', $addResult->getErrorMessages()),
-                $dataResult
-            );
-        }
-
-        $dataResult = ['query' => $query, 'data' => $data];
-        $errorMessage = 'Данные для обновления не найдены';
-        $pkName = $this->getPkName();
-        if (empty($pkName)) {
-            return new OperationResult(
-                $errorMessage,
-                $dataResult
-            );
+            /**
+             * @psalm-suppress PossiblyNullArrayOffset
+             */
+            $data[$pkName] = $id;
+            return new OperationResult(null, $dataResult, $id);
         }
 
         $bxQuery = BxQueryAdapter::init($query);
-        $pkListForUpdate = $this->getPkValuesByQuery($bxQuery);
-
-        if (empty($pkListForUpdate)) {
+        $pkListForSave = $this->getPkValuesByQuery($bxQuery);
+        if (empty($pkListForSave)) {
             return new OperationResult(
-                $errorMessage,
+                'Данные для обновления не найдены',
                 $dataResult
             );
         }
 
         $mainResult = null;
-        foreach ($pkListForUpdate as $pkValue) {
-            $item = $this->initItem($data, (int)$pkValue);
-            $bxResult = $item->save();
-            $saveResult = $bxResult->isSuccess() ?
-                new OperationResult('', $dataResult, $pkValue) :
-                new OperationResult(implode(', ', $bxResult->getErrorMessages()), $dataResult, $pkValue);
+        foreach ($pkListForSave as $id) {
+            /**
+             * @psalm-suppress UndefinedClass
+             */
+            $isSuccess = (bool)$iblockElementInst->Update($id, $dataForSave, $this->useWorkflow);
+
+            /**
+             * @psalm-suppress UndefinedClass
+             */
+            $updateResult = new OperationResult(
+                $isSuccess ? null : $iblockElementInst->LAST_ERROR,
+                $dataResult,
+                $id
+            );
 
             if ($mainResult instanceof OperationResultInterface) {
-                $mainResult->addNext($saveResult);
+                $mainResult->addNext($updateResult);
             } else {
-                $mainResult = $saveResult;
+                $mainResult = $updateResult;
             }
         }
 
         return $mainResult instanceof PkOperationResultInterface ?
             $mainResult :
             new OperationResult('Данные для сохранения не найдены', $dataResult);
+    }
+
+    /**
+     * @return string
+     * @throws SystemException
+     */
+    public function getSourceName(): string
+    {
+        return (string)$this->getIblockId();
+    }
+
+    /**
+     * @param QueryCriteriaInterface|null $query
+     * @return int
+     * @throws SystemException
+     */
+    public function getDataCount(QueryCriteriaInterface $query = null): int
+    {
+        $pkName = $this->getPkName();
+        $params = empty($query) ? [] : BxQueryAdapter::init($query)->toArray();
+        $params['filter']['IBLOCK_ID'] = $this->getIblockId();
+
+        /**
+         * @psalm-suppress UndefinedClass
+         */
+        return (int) CIBlockElement::GetList(
+            [],
+            $params['filter'],
+            false,
+            false,
+            [$pkName]
+        )->SelectedRowsCount();
+    }
+
+    /**
+     * @param BxQueryAdapter $bxQuery
+     * @return array
+     * @throws SystemException
+     */
+    protected function getPkValuesByQuery(BxQueryAdapter $bxQuery): array
+    {
+        $pkName = $this->getPkName();
+        if ($bxQuery->isEqualPkQuery($pkName)) {
+            $result = $bxQuery->getPkValueFromQuery($pkName, CompareRuleInterface::EQUAL);
+
+            return empty($result) ? [] : (array)$result;
+        }
+
+        if (empty($pkName)) {
+            return [];
+        }
+
+        $params = $bxQuery->toArray();
+        $params['filter']['IBLOCK_ID'] = $this->getIblockId();
+
+        /**
+         * @psalm-suppress UndefinedClass
+         */
+        $resSelect = CIBlockElement::GetList([], $params['filter'], false, false, [$pkName]);
+        $result = [];
+        while ($item = $resSelect->Fetch()) {
+            $result[] = $item[$pkName];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param QueryCriteriaInterface $query
+     * @return OperationResultInterface
+     * @throws SystemException
+     */
+    public function remove(QueryCriteriaInterface $query): OperationResultInterface
+    {
+        $bxQuery = BxQueryAdapter::init($query);
+        $pkListForDelete = $this->getPkValuesByQuery($bxQuery);
+        if (empty($pkListForDelete)) {
+            return new OperationResult('Данные для удаления не найдены', ['query' => $query]);
+        }
+
+        $mainResult = null;
+        $dataResult = ['query' => $query];
+        foreach ($pkListForDelete as $pkValue) {
+            /**
+             * @psalm-suppress UndefinedClass
+             */
+            $isSuccess = CIBlockElement::Delete($pkValue);
+            $updateResult = $isSuccess ?
+                new OperationResult('', $dataResult, $pkValue) :
+                new OperationResult("Ошибка удаление элемента #$pkValue", $dataResult, $pkValue);
+
+            if ($mainResult instanceof OperationResultInterface) {
+                $mainResult->addNext($updateResult);
+            } else {
+                $mainResult = $updateResult;
+            }
+        }
+
+        return $mainResult instanceof OperationResultInterface ?
+            $mainResult :
+            new OperationResult('Данные для удаления не найдены', $dataResult);
+    }
+
+    /**
+     * @return bool
+     * @throws SqlQueryException
+     */
+    public function startTransaction(): bool
+    {
+        Application::getConnection()->startTransaction();
+        return true;
+    }
+
+    /**
+     * @return bool
+     * @throws SqlQueryException
+     */
+    public function commitTransaction(): bool
+    {
+        Application::getConnection()->commitTransaction();
+        return true;
+    }
+
+    /**
+     * @return bool
+     * @throws SqlQueryException
+     */
+    public function rollbackTransaction(): bool
+    {
+        Application::getConnection()->rollbackTransaction();
+        return true;
     }
 }
